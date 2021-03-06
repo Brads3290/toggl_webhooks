26ea40ed39dc5d07dc86a36db71884ff3abec05f
Brad S <brad@BRAD-LAPTOP.local>
2021-03-06 13:35:15 +1100

    Finished basic (but untested) functionality for event client

--- toggl/toggl_eventclient.py
+++ toggl/toggl_eventclient.py
@@ -1,15 +1,16 @@
 import signal
 import asyncio
 from toggl.toggl_websocket import TogglSocket, TogglSocketMessage
-from typing import Union
+from typing import Union, List, Callable, Coroutine, Tuple, Any
 
 
-WS_ENDPOINT = "wss://track.toggl.com/stream"
-WS_ORIGIN = "https://track.toggl.com"
+_WS_ENDPOINT = "wss://track.toggl.com/stream"
+_WS_ORIGIN = "https://track.toggl.com"
+_MESSAGE_HANDLER = Callable[[str, str, TogglSocketMessage], Coroutine[Any, Any, Any]]
 
 
 class TogglClient:
-    def __init__(self, api_token: str, ws_endpoint=WS_ENDPOINT, ws_origin=WS_ORIGIN):
+    def __init__(self, api_token: str, ws_endpoint=_WS_ENDPOINT, ws_origin=_WS_ORIGIN):
         self.__api_token = api_token
         self.__ws_endpoint = ws_endpoint
         self.__ws_origin = ws_origin
@@ -17,6 +18,8 @@ class TogglClient:
         self.__should_run = True
         self.__run_task: Union[None, asyncio.Task] = None
         self.__ws_client = TogglSocket(self.__ws_endpoint, self.__ws_origin)
+
+        self.__handlers: List[Tuple[str, str, _MESSAGE_HANDLER]] = []
         return
 
     async def open(self):
@@ -31,6 +34,10 @@ class TogglClient:
 
         await self.start()
 
+        while self.__should_run and not self.__run_task.done():
+            await asyncio.sleep(0.1)
+
+        await self.wait()
         return
 
     async def start(self):
@@ -47,6 +54,9 @@ class TogglClient:
         await self.wait()
         return
 
+    def handle(self, action: str, method: str, handler: _MESSAGE_HANDLER):
+        self.__handlers.append((action, method, handler))
+
     def is_open(self):
         return self.__ws_client.is_open()
 
@@ -55,10 +65,39 @@ class TogglClient:
             raise Exception('TogglClient attempted initialisation before .open() was called.')
 
         await self.__ws_client.authenticate(self.__api_token)
-
         return
 
     async def __run(self):
+        if not self.is_open():
+            raise Exception('ToggleClient attempted to run before .open() was called.')
+
+        if not self.__ws_client.is_authenticated():
+            await self.__ws_client.authenticate(self.__api_token)
+
+        while self.__should_run:
+            msg_task = asyncio.create_task(self.__ws_client.next_message())
+
+            while self.__should_run and not msg_task.done():
+                await asyncio.sleep(0.1)
+
+            if msg_task.done() and msg_task.result() is not None:
+                await self.__message_handler(msg_task.result())
+
+        return
+
+    async def __message_handler(self, msg: TogglSocketMessage):
+        action = msg.get_action()
+        model = msg.get_model()
+
+        tasks = []
+        for candidate in self.__handlers:
+            a, m, h = candidate
+
+            if a == action and m == model:
+                tasks.append(asyncio.create_task(h(action, model, msg)))
+
+        for t in tasks:
+            await t
 
         return
 
